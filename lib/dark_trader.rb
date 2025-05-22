1c1a0530a9ef91518a28f11092c304f8105a2ce0
melody-ste <melody_stephan@icloud.com>
2025-05-22 15:34:42 +0200

    exo dark_trader

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -1,45 +1,36 @@
 require 'nokogiri'
-require 'httparty'
+require 'open-uri'
 require 'pry'
 
 puts "Chargement de la page CoinMarketCap"
-url = 'https://coinmarketcap.com/' #page à scrapper
-response = HTTParty.get(url)          # fait une requête GET
-html_content = response.body          # récupère le HTML
+url = 'https://coinmarketcap.com/all/views/all/' #page à scrapper
+html_content = URI.open(url).read          
 doc = Nokogiri::HTML(html_content)   # analyse le HTML avec Nokogiri
 puts "Page chargée"
 
-
 puts "Extraction des données"
-crypto_rows = doc.xpath('//table//tbody/tr')
-# Sélectionne tous les éléments <tr> (table row) dans <tbody> dans une <table>
-# Chaque <tr> correspond à une crypto
 
 crypto_array = []
 
-# boucle pour extraire les données
-crypto_rows.each do |row|
- 
-  symbol_element = row.at_xpath('.//p[contains(@class, "coin-item-symbol")]')   #//*[@id="__next"]/div[2]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/thead/tr/th[3]
-  
-  # puts "#{symbol_element}" 
+doc.xpath('//table//tbody/tr').each do |row|
 
-  price_element = row.at_xpath('.//td[contains(@class, "price")]//span') 
+  begin
+    symbol_element = row.at_xpath('./td[2]/div/a[2]')&.text&.strip || "N/A"
+    price_element_text = row.at_xpath('./td[5]/div/span')&.text&.strip || "N/A"
+    
+    price = if price_element_text == "N/A"
+              "N/A"
+            else
+              price_element_text.gsub(/[^\d\.]/, '').to_f
+            end
 
-  # puts "#{price_element}" 
-
-  # si il y a les deux infos 
-  if symbol_element && price_element
-    symbol = symbol_element.text.strip #récup juste le texte
-    price = price_element.text.strip.gsub(/[^\d\.]/, '').to_f  # récup juste le nombre
-
-    crypto_hash = { symbol => price.round(2) } 
-    crypto_array << crypto_hash  # ajoute le hash au tableau vide dans haut
-
-    puts " #{symbol} : #{price.round(2)} USD"
+   crypto_array << { symbol_element => price }
+   puts "#{symbol_element} : #{price} USD"       
+  rescue => e
+    crypto_array << { "N/A" => "N/A" }
   end
 end
-
-
-puts "\n Résultat final : #{crypto_array.size} cryptos récupérées"
-puts crypto_array
+ 
+# puts " #{symbol} : #{price} USD"
+  
+# puts "\n Résultat final : #{crypto_array.size} cryptos récupérées"
